8e428cc596e9f0070c3b04cef80c8a1f7e309075
sgasperi <sgasperi@ucsc.edu>
2018-03-01 16:14:48 +0100

    lo snippet per FILE

---
+++ src/FileManager.java
@@ -0,0 +1,53 @@
+package /*AAAAAAAAAAAAAAAAAAAAAA*/;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FileManager {
+	
+	public static void createEmptyFile() {
+		Path filePath = Paths.get("./output.txt");
+		try (BufferedWriter writer = Files.newBufferedWriter(filePath)) {   
+		    System.out.println("./output.txt will be empty");
+		} catch (IOException e) {
+			System.out.println("!!! PROBLEM WITH THE OUTPUT FILE !!!\n" + e.getMessage());
+		}
+	}
+
+	public static void printLines(List<String> lines) {
+		Path filePath = Paths.get("./output.txt");
+		try (BufferedWriter writer = Files.newBufferedWriter(filePath)) {   
+		    writer.write(lines.stream().reduce((sum,currLine) ->  sum + "\n"  + currLine).get());
+		    System.out.println("text saved in ./output.txt");
+		} catch (IOException e) {
+			System.out.println("!!! PROBLEM WITH THE OUTPUT FILE !!!\n" + e.getMessage());
+		}
+	}
+	
+	public static List<String> getLines(String path) {
+		try (Stream<String> stream = Files.lines(Paths.get(path))) {
+			return stream.collect(Collectors.toList());
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	public static List<Object/*AAAAAAAAAAAAAAAAA*/> splitLines(List<String> lines){
+		StringTokenizer st;
+		for(String line : lines){
+			st = new StringTokenizer(line);
+			while (st.hasMoreTokens()) {
+				String s = st.nextToken();
+				/*AAAAAAAAAAAAA*/
+			}
+		}
+	}
+	
+}
+
