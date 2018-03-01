0197c8fe2a727ff3f8372af951747f2807b163cb
Francesco Fiacco <fiacco.fra@gmail.com>
2018-03-01 19:38:06 +0000

    vehicle and ride classes

---
+++ src/skillsBelowLowerBoundException/Ride.java
@@ -0,0 +1,57 @@
+package skillsBelowLowerBoundException;
+
+public class Ride {
+	private int a; 
+	private int b; 
+	private int x; 
+	private int y; 
+	private int s; 
+	private int f; 
+
+	public Ride(int a, int b, int x, int y, int s, int f) {
+		super();
+		this.a = a;
+		this.b = b;
+		this.x = x;
+		this.y = y;
+		this.s = s;
+		this.f = f;
+	}
+	
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public int getB() {
+		return b;
+	}
+	public void setB(int b) {
+		this.b = b;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getS() {
+		return s;
+	}
+	public void setS(int s) {
+		this.s = s;
+	}
+	public int getF() {
+		return f;
+	}
+	public void setF(int f) {
+		this.f = f;
+	}			
+}
---
+++ src/skillsBelowLowerBoundException/Vehicle.java
@@ -0,0 +1,60 @@
+package skillsBelowLowerBoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vehicle {
+	private boolean isFree;
+	private int x;
+	private int y;
+	private List<Ride> rides;
+	private int freeCount;
+	
+	public Vehicle(int x, int y) {
+		super();
+		this.isFree = true;
+		this.x = x;
+		this.y = y;
+		this.rides = new ArrayList<Ride>();
+		this.freeCount = 0;
+	}
+	
+	public int getFreeCount() {
+		return freeCount;
+	}
+
+	public void setFreeCount(int freeCount) {
+		this.freeCount = freeCount;
+		if (freeCount == 0)
+			isFree = true;
+	}
+
+	public void setFree(boolean isFree) {
+		this.isFree = isFree;
+	}
+
+	public boolean isFree() {
+		return isFree;
+	}
+	public void setisFree(boolean isFree) {
+		this.isFree = isFree;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public List<Ride> getRides() {
+		return rides;
+	}
+	public void setRides(List<Ride> rides) {
+		this.rides = rides;
+	}
+}
